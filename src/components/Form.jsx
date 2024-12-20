3f6c9bab96437007301608d5b2066496aa1dcd9f
Emerson Frasure <emersonfrasure@gmail.com>
2024-12-20 11:46:42 -0500

    Conditional rendering for list, adds get recipe button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,9 +25,28 @@ function Form() {
                 <button>Add Ingredient</button>
             </form>
 
-            <ul>
-                {ingredientList}
-            </ul>
+            <section className="ingredients-list">
+                <h2>Ingredients on hand:</h2>
+
+                {ingredientList.length ? 
+                    <ul aria-live="polite">
+                        {ingredientList}
+                        {ingredientList.length < 3 && <p>Add {3-ingredientList.length} more ingredients...</p>}
+                    </ul>
+                    :
+                    <p>To get started add ingredients above...</p>    
+                }
+            </section>
+            {ingredientList.length >= 3 &&
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            }
+            
         </main>
     )
 }
